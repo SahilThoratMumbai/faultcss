f35abe45c9faf510f0938b4b15d3c64a042a6c06
Sahil Thorat <sahil.thorat68@gmail.com>
2023-10-11 19:32:13 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App"
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Wagh from "./components/Wagh";
+                   
+
+ReactDom.render(
+  <>
+  <BrowserRouter> <App/>
+  <Wagh/>
+  </BrowserRouter>
+    
+  </>
+  ,document.getElementById("root")
+)
